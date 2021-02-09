1c25d530bb02e8165d5c5cf5b5057797e3654b66
Dr.ReMain <remain@divergers.com>
2021-02-09 21:58:06 +0800

    feat: Add plugin-pwd

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,73 +1,84 @@
 module.exports = {
-  title: "DrReMain's Lab",
-  // tagline: 'Adversity makes a man wise, not rich.',
-  url: "https://drremain.cn",
-  baseUrl: "/",
-  onBrokenLinks: "throw",
-  onBrokenMarkdownLinks: "warn",
-  favicon: "img/favicon.ico",
-  organizationName: "drremain", // Usually your GitHub org/user name.
-  projectName: "DrReMain", // Usually your repo name.
-  themeConfig: {
-    navbar: {
-      title: "",
-      logo: {
-        alt: "drremain",
-        src: "img/logo.png",
-      },
-      items: [
-        {
-          to: "docs/",
-          activeBasePath: "docs",
-          label: "文章",
-          position: "left",
+    title: "DrReMain's Lab",
+    // tagline: 'Adversity makes a man wise, not rich.',
+    url: "https://drremain.cn",
+    baseUrl: "/",
+    onBrokenLinks: "throw",
+    onBrokenMarkdownLinks: "warn",
+    favicon: "img/favicon.ico",
+    organizationName: "drremain", // Usually your GitHub org/user name.
+    projectName: "DrReMain", // Usually your repo name.
+    themeConfig: {
+        navbar: {
+            title: "",
+            logo: {
+                alt: "drremain",
+                src: "img/logo.png",
+            },
+            items: [
+                {
+                    to: "docs/",
+                    activeBasePath: "docs",
+                    label: "文章",
+                    position: "left",
+                },
+                // {to: 'blog', label: 'Blog', position: 'left'},
+                {
+                    href: "https://github.com/DrReMain",
+                    label: "GitHub",
+                    position: "right",
+                },
+            ],
         },
-        // {to: 'blog', label: 'Blog', position: 'left'},
-        {
-          href: "https://github.com/DrReMain",
-          label: "GitHub",
-          position: "right",
+        footer: {
+            style: "dark",
+            // links: [
+            //   {
+            //     title: '社区',
+            //     items: [
+            //       {
+            //         label: '掘金',
+            //         href: 'https://juejin.cn/user/2858385962182077',
+            //       },
+            //       {
+            //         label: 'stackoverflow',
+            //         href: 'https://stackoverflow.com/users/12681046/dr-remain',
+            //       },
+            //     ],
+            //   },
+            // ],
+            copyright: `Copyright © ${new Date().getFullYear()} DrReMain.`,
         },
-      ],
     },
-    footer: {
-      style: "dark",
-      // links: [
-      //   {
-      //     title: '社区',
-      //     items: [
-      //       {
-      //         label: '掘金',
-      //         href: 'https://juejin.cn/user/2858385962182077',
-      //       },
-      //       {
-      //         label: 'stackoverflow',
-      //         href: 'https://stackoverflow.com/users/12681046/dr-remain',
-      //       },
-      //     ],
-      //   },
-      // ],
-      copyright: `Copyright © ${new Date().getFullYear()} DrReMain.`,
-    },
-  },
-  presets: [
-    [
-      "@docusaurus/preset-classic",
-      {
-        docs: {
-          sidebarPath: require.resolve("./sidebars.js"),
-          // Please change this to your repo.
-          editUrl: "https://github.com/DrReMain/DrReMain/issues",
-        },
-        blog: {
-          showReadingTime: true,
-          // Please change this to your repo.
-          editUrl: "https://github.com/DrReMain/DrReMain/issues",
-        },
-        theme: {
-          customCss: require.resolve("./src/css/custom.css"),
-        },
-      },
+    presets: [
+        [
+            "@docusaurus/preset-classic",
+            {
+                docs: {
+                    sidebarPath: require.resolve("./sidebars.js"),
+                    // Please change this to your repo.
+                    editUrl: "https://github.com/DrReMain/DrReMain/issues",
+                },
+                blog: {
+                    showReadingTime: true,
+                    // Please change this to your repo.
+                    editUrl: "https://github.com/DrReMain/DrReMain/issues",
+                },
+                theme: {
+                    customCss: require.resolve("./src/css/custom.css"),
+                },
+                sitemap: {
+                    cacheTime: 600 * 1000,
+                    changefreq: 'weekly',
+                    priority: 0.5,
+                    trailingSlash: false,
+                }
+            },
+        ],
     ],
-  ],
+    plugins: [
+        ['@docusaurus/plugin-pwa', {
+            debug: true
+        }]
+    ]
 };
