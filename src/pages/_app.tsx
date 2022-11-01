07a23c93e043e2a9417c2f3392c2ab36b3e25b66
shijiao <51071534@qq.com>
2022-11-01 09:14:22 +0800

    fix: viewport

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,7 @@
 import '../styles/globals.css';
 import { useEffect } from 'react';
 import type { AppProps } from 'next/app';
+import Head from 'next/head';
 import { Analytics } from '@vercel/analytics/react';
 
 export default function App({ Component, pageProps }: AppProps) {
@@ -14,6 +15,9 @@ export default function App({ Component, pageProps }: AppProps) {
 
   return (
     <>
+      <Head>
+        <meta name="viewport" content="width=device-width,initial-scale=1" />
+      </Head>
       <Component {...pageProps} />
       <Analytics />
     </>
